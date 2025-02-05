da307ec295dab510f0ee492d237d05869daf5a55
Dani Cairns <dpcairns@mac.lan>
2025-02-05 10:49:25 -0800

    add tested findPrimesBelowN method to list all primes under a given value

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,6 +1,43 @@
 class Sieve {
   NthPrime(n) {
-    throw new Error("unimplemented");
+    console.log({ n})
+
+    let temp = 0;
+    // starting with 2,
+    while (temp < n) {
+      console.log(arr[n])
+    }
+  }
+  findPrimesBelowN(n) {
+    const arr = new Array(n - 1).fill(2).reduce((acc, curr, index) => {
+      if (index === 0) return acc;
+
+      return [...acc, acc[index - 1] + 1];
+    }, [2])
+
+    const primes = []
+    let anchorPrime = 2;
+    let anchorPrimeIndex = 0;
+    while (anchorPrime < n) {
+      for (let i = anchorPrimeIndex; i < arr.length; i += anchorPrime) {
+        if (i > anchorPrimeIndex + 1) {
+          arr.splice(i, 1, 'marked')
+        }
+      }
+
+      anchorPrime = arr.find((item, index) => {
+        if (!!item && item > anchorPrime) {
+          anchorPrimeIndex = index;
+          return item
+        }
+      })
+
+      if (anchorPrime) {
+        primes.push(anchorPrime)
+      }
+    }
+    
+    return arr.filter(item => item !== 'marked');
   }
 }
 
--- javascript/Sieve/sieve.test.js
+++ javascript/Sieve/sieve.test.js
@@ -1,16 +1,19 @@
 const Sieve = require("./sieve");
+const primesBelow1000 = require("./primes-below-1000");
 
 describe("Sieve", () => {
   test("valid results", () => {
     const sieve = new Sieve();
-    expect(sieve.NthPrime(0)).toBe(2);
-    expect(sieve.NthPrime(19)).toBe(71);
-    expect(sieve.NthPrime(99)).toBe(541);
-    expect(sieve.NthPrime(500)).toBe(3581);
-    expect(sieve.NthPrime(986)).toBe(7793);
-    expect(sieve.NthPrime(2000)).toBe(17393);
-    expect(sieve.NthPrime(1000000)).toBe(15485867);
-    expect(sieve.NthPrime(10000000)).toBe(179424691);
+    expect(sieve.findPrimesBelowN(20)).toEqual([2, 3, 5, 7, 11, 13, 17, 19]);
+    expect(sieve.findPrimesBelowN(1000)).toEqual(primesBelow1000);
+    // expect(sieve.NthPrime(0)).toBe(2);
+    // expect(sieve.NthPrime(19)).toBe(71);
+    // expect(sieve.NthPrime(99)).toBe(541);
+    // expect(sieve.NthPrime(500)).toBe(3581);
+    // expect(sieve.NthPrime(986)).toBe(7793);
+    // expect(sieve.NthPrime(2000)).toBe(17393);
+    // expect(sieve.NthPrime(1000000)).toBe(15485867);
+    // expect(sieve.NthPrime(10000000)).toBe(179424691);
     //expect(sieve.NthPrime(100000000)).toBe(2038074751); not required, just a fun challenge
   });
 });
